3cc6326efc7c93573b246ec768dba36b6729c787
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2023-09-14 17:58:25 -0700

    Changed image margins
    Removed top and bottom margin and added small left and right margin values.

--- src/data.js
+++ src/data.js
@@ -32,15 +32,3 @@ const data = [
 ]
 
 export default data
-
-// Photos from Unsplash
-//     (can use these URLs as your img src)
-
-// Japan
-// https://source.unsplash.com/WLxQvbMyfas
-
-// Australia
-// https://source.unsplash.com/JmuyB_LibRo
-
-// Norway
-// https://source.unsplash.com/3PeSjpLVtLg
